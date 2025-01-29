767283d5eedc7c75113a6b3debee3c191e1f1e70
Franck Wallace <franckwallace.fw@outlook.com>
2025-01-28 23:04:27 -0300

    Initial Commit

---
+++ ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    internal class Funcionario
+    {
+        // Criando o campo "_tipo" que será usado no Construtor "Funcionario"
+        // 0 - Funcionario
+        // 1 - Diretor
+        // 2 - Designer
+        // N - ...
+        // private int _tipo;
+
+
+        // Criando novas Propriedades para o funcionário
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        // Construtor "Funcionario" que indica qual tipo de funcionário o objeto criado faz parte
+        //public Funcionario(int tipo) 
+        //{
+        //    _tipo = tipo;
+        //}
+
+        // Método que retorna a bonificação
+        public double GetBonificacao() 
+        {
+            //if (_tipo == 1)
+            //{
+            //    return Salario * 1.0;
+            //}
+
+            //if (_tipo == 2)
+            //{
+            //    return Salario * 0.15;
+            //}
+
+            return Salario * 0.1;
+        }
+
+
+    }
+}
---
+++ ByteBank/ByteBank/Program.cs
@@ -0,0 +1,42 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario geovana = new Funcionario();
+
+            geovana.Nome = "Geovana";
+            geovana.CPF = "123.456.789-00";
+            geovana.Salario = 3500;
+
+            Diretor larissa = new Diretor();
+
+            larissa.Nome = "Larissa";
+            larissa.CPF = "000.999.789-00";
+            larissa.Salario = 6125;
+
+
+            //
+            gerenciador.Registar(geovana);
+            gerenciador.Registar(larissa);
+
+
+            Console.WriteLine("Nome: " + geovana.Nome);
+            Console.WriteLine("CPF: " + geovana.CPF);
+            Console.WriteLine("Salário: R$ " + geovana.Salario);
+            Console.WriteLine("Bonificação: R$ " + geovana.GetBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
